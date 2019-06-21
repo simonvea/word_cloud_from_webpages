f8c0e4ec408b3e8fe7ea853712fcc4a5a6c3f791
Simon Opheim <simon.opheim@gmail.com>
2019-06-21 17:31:09 +0200

    working server with express post requests

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 'use strict'
 
 const getKeyWordsFromUrls = require('./get-keywords');
-const helpers = require('./helpers')
-const urls = require('./urls.json');
-const htmlElement = ".import-decoration"
+const helpers = require('./helpers');
+const express = require('express');
 
-getKeyWordsFromUrls(urls, htmlElement).then(wordCounts => {
-    const wordCountTotal = helpers.combineCountsFromArray(wordCounts);
-    const cleanedWordCount = helpers.getRelevantKeyWords(wordCountTotal);
-    console.log(cleanedWordCount);
-}).catch(error => console.log(error));
+//Server
+const app = express();
+const port = 3000;
 
+app.use(express.static('public'));
 
+app.use(express.json())
+app.post('/keywords', (req, res) => {
+    const data = req.body;
+    const urls = req.body.urls;
+    const htmlElement = req.body.htmlElement;
+    getKeyWordsFromUrls(urls, htmlElement)
+        .then(wordCounts => {
+            const wordCountTotal = helpers.combineCountsFromArray(wordCounts);
+            const cleanedWordCount = helpers.getRelevantKeyWords(wordCountTotal);
+            res.send(cleanedWordCount)
+        }).catch(error => console.log(error));
+    console.log(data);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
